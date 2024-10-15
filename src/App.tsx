279f221a8c4e17b23fccf426128c6b123c7e83b1
victorpantarotti <victorpantarotti@gmail.com>
2024-10-15 11:39:16 -0300

    Start Project and Initial Config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "./components/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <p>Hello World</p>
+    </>
+  );
+}
+
+export default App;

93734280985d0b0eaa475f4ff93febdb3b46d180
victorpantarotti <victorpantarotti@gmail.com>
2024-12-05 17:59:18 -0300

    Small change to AntDTheme

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,32 @@ import Slides from "./pages/Slides";
 import preferences from "./settings/preferences";
 import slides from "./settings/slides";
 
-const AntDTheme = {
-  token: {
-    colorBgElevated: preferences.colors.antdTheme?.token?.colorBgElevated || "var(--secondaryColor)",
-    colorText: preferences.colors.antdTheme?.token?.colorText || "var(--textColor)",
-    colorTextPlaceholder: preferences.colors.antdTheme?.token?.colorTextPlaceholder || "var(--textColor)",
-    colorIcon: preferences.colors.antdTheme?.token?.colorIcon || "var(--textColor)",
-    colorIconHover: preferences.colors.antdTheme?.token?.colorIconHover || "var(--textColor)",
-  },
-  components: {
-    Input: {
-      activeBg: preferences.colors.antdTheme?.components?.Input?.activeBg|| "var(--secondaryColor)",
-      colorBgContainer: preferences.colors.antdTheme?.components?.Input?.colorBgContainer || "var(--primaryColor)",
-      colorBorder: preferences.colors.antdTheme?.components?.Input?.colorBorder || "var(--primaryColor)"
+const getAntDTheme = () => {
+  const token = preferences.colors.antdTheme?.token;
+  const components = preferences.colors.antdTheme?.components;
+
+  return {
+    token: {
+      colorBgElevated: token?.colorBgElevated || "var(--secondaryColor)",
+      colorText: token?.colorText || "var(--textColor)",
+      colorTextPlaceholder: token?.colorTextPlaceholder || "var(--textColor)",
+      colorIcon: token?.colorIcon || "var(--textColor)",
+      colorIconHover: token?.colorIconHover || "var(--textColor)",
+    },
+    components: {
+      Input: {
+        activeBg: components?.Input?.activeBg|| "var(--secondaryColor)",
+        colorBgContainer: components?.Input?.colorBgContainer || "var(--primaryColor)",
+        colorBorder: components?.Input?.colorBorder || "var(--primaryColor)"
+      }
     }
-  }
+  };
 };
 
 function App() {
   const { showLoading } = useGlobalContext();
   const { isEnd, slideIndex } = useSlidesContext();
+  const AntDTheme = getAntDTheme();
   const currentSlide = slides[slideIndex as keyof object];
 
   useEffect(() => showLoading("reset"), []);
